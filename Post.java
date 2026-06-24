5c420cf5002e0498e9b84033f10aa9804f83929b
SR 55/2024 Bokan Nemanja <140839491+bolenceeee@users.noreply.github.com>
2026-06-24 11:26:18 +0200

    Update Post.java

--- Post.java
+++ Post.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
-public class Post {
-    public int id;
-    public String title;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
 }
